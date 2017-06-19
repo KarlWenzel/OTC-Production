714eb6277337ecc7929f02413f2ef121287aa06d
KarlWenzel <karl.wenzel@wenzeltech.com>
2017-06-19 17:30:06 -0500

    making hyperbolic curve now

--- main.R
+++ main.R
@@ -1,10 +1,18 @@
 library(RODBC)
 library(lubridate)
+library(reshape2)
 
-setwd("d:/git/OTC-Production/data")
+#################
+# CONFIGURATION #
+#################
 
+data_folder = "d:/git/OTC-Production/data"
+reports_folder = "d:/git/OTC-Production/reports"
 sql_dsn = "OTC"
-conn = odbcConnect(sql_dsn)
+first_time_run = FALSE
+
+# If this isn't the first time to run this program, you may select individual processes to 
+# be executed below.
 
 load.exp_gpexempt.dat = FALSE
 load.exp_gpoper.dat = FALSE
@@ -12,8 +20,31 @@ load.exp_gpqtrat.dat = FALSE
 load.exp_gplease.dat = FALSE
 load.exp_gph_reports_12.dat = FALSE
 load.exp_gph_reports_36.dat = FALSE
+process.raw.data = FALSE
+build.hyperbolic.model = TRUE
+
+# Apply configuration, get connection to database
+
+setwd(data_folder)
+conn = odbcConnect(sql_dsn)
+
+if (first_time_run) {
+  load.exp_gpexempt.dat = TRUE
+  load.exp_gpoper.dat = TRUE
+  load.exp_gpqtrat.dat = TRUE
+  load.exp_gplease.dat = TRUE
+  load.exp_gph_reports_12.dat = TRUE
+  load.exp_gph_reports_36.dat = TRUE
+  process.raw.data = TRUE
+  build.hyperbolic.model = TRUE
+}
+
+#################
+# Load Raw Data #
+#################
 
 if (load.exp_gpexempt.dat) {
+  print("Loading exp_gpexempt.dat");
   sqlQuery(conn, "DELETE FROM exp_gpexempt")
   dat.file = file("exp_gpexempt.dat", "r")
   while ( TRUE ) {
@@ -36,6 +67,7 @@ if (load.exp_gpexempt.dat) {
 }
 
 if (load.exp_gpoper.dat) {
+  print("Loading exp_gpoper.dat");
   sqlQuery(conn, "DELETE FROM exp_gpoper")
   dat.file = file("exp_gpoper.dat", "r")
   while ( TRUE ) {
@@ -57,6 +89,7 @@ if (load.exp_gpoper.dat) {
 }
 
 if (load.exp_gpqtrat.dat) {
+  print("Loading exp_gpqtrat.dat");
   sqlQuery(conn, "DELETE FROM exp_gpqtrat")
   dat.file = file("exp_gpqtrat.dat", "r")
   while ( TRUE ) {
@@ -85,6 +118,7 @@ if (load.exp_gpqtrat.dat) {
 }
 
 if (load.exp_gplease.dat) {
+  print("Loading exp_gplease.dat");
   sqlQuery(conn, "DELETE FROM exp_gplease")
   dat.file = file("exp_gplease.dat", "r")
   while ( TRUE ) {
@@ -116,6 +150,7 @@ if (load.exp_gplease.dat) {
 }
 
 if (load.exp_gph_reports_12.dat) {
+  print("Loading exp_gph_reports.dat");
   sqlQuery(conn, "DELETE FROM exp_gph_reports_12")
   dat.file = file("exp_gph_reports_12.dat", "r")
   while ( TRUE ) {
@@ -149,6 +184,7 @@ if (load.exp_gph_reports_12.dat) {
 }
 
 if (load.exp_gph_reports_36.dat) {
+  print("Loading exp_gph_reports_36.dat");
   sqlQuery(conn, "DELETE FROM exp_gph_reports_36")
   dat.file = file("exp_gph_reports_36.dat", "r")
   while ( TRUE ) {
@@ -179,6 +215,67 @@ if (load.exp_gph_reports_36.dat) {
     sqlSave(conn, exp_gph_reports_36, append=TRUE, rownames=FALSE)
   }
   close(dat.file)
+}
+
+######################
+# Build Tidy Dataset #
+######################
+
+if (process.raw.data) {
+  print("Processing raw data")
+  sqlQuery(conn, "EXEC ProcessFromDTOs") 
+  # Please refer to the ProcessFromDTOs stored procedure for documentation
+}
+
+###############
+# Build Model #
+###############
+
+hyperbolic.curve = function(qi, Di, b, t) {
+  return( qi / (1 + b * Di * t) ^ (1 / b) );
+}
+
+get.curve = function(data, product) {
+  # build curve
+  q = melt(data[data$Product==product, 2+(1:24)])$value
+  q0 = q[1]
+  q1_23 = q[2:24]
+  qi = q1_23[1]
+  Di = -1 * (q1_23[2] - q1_23[1])
+  b = 0.5
+  t = 1:((12*20) - 2) # 20 years worth of months, minus two months (q0 and qi)
+  curve = c(q0, qi, hyperbolic.curve(qi, Di, b, t))
+  # save plot as pdf
+  pdf(file=paste0(reports_folder, "/", product, "-decline-curve.pdf"))
+  plot(t[1:36], curve[1:36], type="l", col="red", xlab="Month", ylab="Normalized Production", ylim=c(0,1))
+  points(1:length(q), q)
+  title(paste0("Normalized ", product, " Production in Horizontal Wells Since Feb 2014"))
+  print(dev.cur())
+  dev.off()
+  return(curve)
+}
+
+get.interval.percents = function(data, product){
+  total.production = 
+}
+
+if (build.hyperbolic.model) {
+  print("Saving production curves")
+  data = sqlQuery(conn, "EXEC SelectAllCurves")
+  
+  # The data obtained from SelectAllCurves is an averaage of normalized well data.  It makes sense
+  # to rescale the data again to a new normalized data set to ensure that data ranges from (0,1]
+  
+  for (i in 1:nrow(data)) {
+    data[i,3:26] = data[i,3:26] / max(data[i,3:26])
+  }
+  write.csv(data, paste0(reports_folder, "/empirical-production-curves.csv"))
+  
+  gas.curve = get.curve(data, "Gas")
+  oil.curve = get.curve(data, "Oil")
+  
+  write.csv(gas.curve, paste0(reports_folder, "/estimated-gas-curve.csv"))
+  write.csv(oil.curve, paste0(reports_folder, "/estimated-oil-curve.csv"))
 }
 
 odbcClose(conn)
